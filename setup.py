f7075e2482775ec28a6a4d43da121c6f32065a70
Mathieu Lecarme <mathieu@garambrogne.net>
2010-01-18 17:39:32 +0100

    cleanup

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ use_setuptools()
 from setuptools import setup
 
 setup(name='imageBundler',
-      version='0.2.1',
+      version='0.2.2',
       license='GPL-3',
       description='Image bundler',
       author='Mathieu Lecarme',
       author_email='mathieu@garambrogne.net',
       url='https://admin.garambrogne.net/projets/bundler',
-      packages=[''],
-      package_dir={'': 'src/'},
-      scripts=['src/bundler'],
+      packages=['bundler'],
+      package_dir={'': 'src'},
+      scripts=['bin/bundler'],
       install_requires=["PIL"]
      )
