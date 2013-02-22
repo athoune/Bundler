96d1f51493126525d46040b300c34863e781dc1a
Mathieu Lecarme <mlecarme@bearstech.com>
2013-02-22 18:48:33 +0100

    PEP8.

--- src/bundler/__init__.py
+++ src/bundler/__init__.py
@@ -30,73 +30,84 @@ import glob
 import os
 import os.path
 import mimetypes
-import subprocess
+
 
 class MultiMap:
-	def __init__(self):
-		self.data = {}
-	def add(self, key, value):
-		self.data.setdefault(key, []).append(value)
-	def __getitem__(self, key):
-		return self.data[key]
-	def __repr__(self):
-		return str(self.data)
-	def keys(self):
-		return self.data.keys()
-	def max(self):
-		return self.data[max(self.data.keys())]
+    def __init__(self):
+        self.data = {}
+
+    def add(self, key, value):
+        self.data.setdefault(key, []).append(value)
+
+    def __getitem__(self, key):
+        return self.data[key]
+
+    def __repr__(self):
+        return str(self.data)
+
+    def keys(self):
+        return self.data.keys()
+
+    def max(self):
+        return self.data[max(self.data.keys())]
+
 
 class Bundle:
-	"""
-	[TODO] using a template for css and html
-	"""
-	def __init__(self):
-		self.images = set()
-		self.byWidth = MultiMap()
-		self.byHeight = MultiMap()
-	def add(self, path, image):
-		img = Img(path, image)
-		self.images.add(img)
-		w,h = image.size
-		self.byWidth.add(w, img)
-		self.byHeight.add(h, img)
-	def __repr__(self):
-		return "<Bundle images:%s byWidth:%s byHeight:%s>" % (str(self.images), str(self.byWidth), str(self.byHeight))
-	def larger(self):
-		return max(self.byWidth.keys())
-	def build(self):
-		height = 0
-		width = 0
-		stuff = self.byHeight.keys()
-		maxWidth = max(self.byWidth.keys())
-		width = maxWidth
-		stuff.sort()
-		stuff.reverse()
-		surface = 0
-		lineH = 0
-		for s in stuff:
-			for img in self.byHeight[s]:
-				w, h = img.image.size
-				surface += w*h
-				if h >lineH : lineH = h
-				if (w + width) > maxWidth:
-					height += lineH
-					width = 0
-					lineH = h
-				width += w
-		height += lineH - stuff[0]
-		print  "size:", maxWidth, height
-		print "optimisation :", (surface *1.0 / (maxWidth * height)) * 100
-		big = Image.new("RGBA", (maxWidth, height))
-		height = 0
-		width = 0
-		lineH = 0
-		css = """
+    """
+    [TODO] using a template for css and html
+    """
+    def __init__(self):
+        self.images = set()
+        self.byWidth = MultiMap()
+        self.byHeight = MultiMap()
+
+    def add(self, path, image):
+        img = Img(path, image)
+        self.images.add(img)
+        w, h = image.size
+        self.byWidth.add(w, img)
+        self.byHeight.add(h, img)
+
+    def __repr__(self):
+        return "<Bundle images:%s byWidth:%s byHeight:%s>" % (str(self.images), str(self.byWidth), str(self.byHeight))
+
+    def larger(self):
+        return max(self.byWidth.keys())
+
+    def build(self):
+        height = 0
+        width = 0
+        stuff = self.byHeight.keys()
+        maxWidth = max(self.byWidth.keys())
+        width = maxWidth
+        stuff.sort()
+        stuff.reverse()
+        surface = 0
+        lineH = 0
+        for s in stuff:
+            for img in self.byHeight[s]:
+                w, h = img.image.size
+                surface += w * h
+                if h > lineH:
+                    lineH = h
+                if (w + width) > maxWidth:
+                    height += lineH
+                    width = 0
+                    lineH = h
+                width += w
+        height += lineH - stuff[0]
+        print  "size:", maxWidth, height
+        print "optimisation :", (surface * 1.0 / (maxWidth * height)) * 100
+        big = Image.new("RGBA", (maxWidth, height))
+        height = 0
+        width = 0
+        lineH = 0
+        css = """
 .redborder {
-	border: 1px dotted red;
+    border: 1px dotted red;
 }
-		"""
-		html = """
+        """
+        html = """
 <html>
 <head>
 <link rel="stylesheet" id="csspersolink" type="text/css" href="bundle.css" />
@@ -104,90 +115,96 @@ class Bundle:
 <body bgcolor="lime">
 <h1>Bundler</h1>
 
-		"""
-		for s in stuff:
-			print "height:", s
-			for img in self.byHeight[s]:
-				w, h = img.image.size
-				if h >lineH : lineH = h
-				if (width + w) > maxWidth:
-					height += lineH
-					width = 0
-					lineH = h
-				#print "pasting", big, (w,h), (width, height)
-				big.paste(img.image, (width, height))
-				idt = img.path.replace("/","_")
-				idt = idt.replace(" ", "_")
-				ids = idt.split('.')
-				if len(ids) > 1:
-					idt = ".".join(ids[:-1])
-				css += """
+        """
+        for s in stuff:
+            print "height:", s
+            for img in self.byHeight[s]:
+                w, h = img.image.size
+                if h > lineH:
+                    lineH = h
+                if (width + w) > maxWidth:
+                    height += lineH
+                    width = 0
+                    lineH = h
+                #print "pasting", big, (w,h), (width, height)
+                big.paste(img.image, (width, height))
+                idt = img.path.replace("/", "_")
+                idt = idt.replace(" ", "_")
+                ids = idt.split('.')
+                if len(ids) > 1:
+                    idt = ".".join(ids[:-1])
+                css += """
 .%s {
-	background: transparent url(bundle.png) no-repeat -%ipx -%ipx;
-	width: %ipx;
-	height: %ipx;
+    background: transparent url(bundle.png) no-repeat -%ipx -%ipx;
+    width: %ipx;
+    height: %ipx;
 }
-				""" % (idt, width, height, w, h)
-				html += """<h2>%s</h2><div class="%s redborder">&nbsp</div>
+                """ % (idt, width, height, w, h)
+                html += """<h2>%s</h2><div class="%s redborder">&nbsp</div>
 """ % (img.path, idt)
-				width += w
-		big.save('bundle.png')
-		f = open('bundle.css', 'w')
-		f.write(css)
-		f.close()
-		html += """
-		</body>
+                width += w
+        big.save('bundle.png')
+        f = open('bundle.css', 'w')
+        f.write(css)
+        f.close()
+        html += """
+        </body>
 </html>
 """
-		f = open('bundle.html', 'w')
-		f.write(html)
-		f.close()
+        f = open('bundle.html', 'w')
+        f.write(html)
+        f.close()
+
 
 def redBorder(im):
-	w,h = im.size
-	w -= 1
-	h -=1
-	draw = ImageDraw.Draw(im)
-	color = 255
-	#rectangle
-	draw.line((0,0, 0,h), fill=color)
-	draw.line((0,h, w,h), fill=color)
-	draw.line((w,h, w,0), fill=color)
-	draw.line((w,0, 0,0), fill=color)
-	del draw
+    w, h = im.size
+    w -= 1
+    h -= 1
+    draw = ImageDraw.Draw(im)
+    color = 255
+    #rectangle
+    draw.line((0, 0, 0, h), fill=color)
+    draw.line((0, h, w, h), fill=color)
+    draw.line((w, h, w, 0), fill=color)
+    draw.line((w, 0, 0, 0), fill=color)
+    del draw
+
 
 class Img:
-	def __init__(self, path, image):
-		self.path = path
-		self.image = image
-	def __repr__(self):
-		return "<Img path:%s image:%s>" % (str(self.path), str(self.image))
+    def __init__(self, path, image):
+        self.path = path
+        self.image = image
+
+    def __repr__(self):
+        return "<Img path:%s image:%s>" % (str(self.path), str(self.image))
+
 
 def bundle_pattern(pattern):
-	b = Bundle()
-	print pattern
-	for name in glob.iglob(pattern):
-		print name
-		im = Image.open(name)
-		#redBorder(im)
-		b.add(name, im)
-	b.build()
+    b = Bundle()
+    print pattern
+    for name in glob.iglob(pattern):
+        print name
+        im = Image.open(name)
+        #redBorder(im)
+        b.add(name, im)
+    b.build()
+
 
 def bundle(folder):
-	if not os.path.isdir(folder):
-		raise Exception, "I need a folder"
-	Image.preinit()
-	mimes = Image.MIME.values()
-	b = Bundle()
-	for stuff in os.listdir(folder):
-		mime = mimetypes.guess_type(stuff)
-		#print mime, stuff
-		if mime[0] in mimes:
-			print stuff
-			path = os.path.join(folder, stuff)
-			im = Image.open(path)
-			b.add(path, im)
-	b.build()
+    if not os.path.isdir(folder):
+        raise Exception("I need a folder")
+    Image.preinit()
+    mimes = Image.MIME.values()
+    b = Bundle()
+    for stuff in os.listdir(folder):
+        mime = mimetypes.guess_type(stuff)
+        #print mime, stuff
+        if mime[0] in mimes:
+            print stuff
+            path = os.path.join(folder, stuff)
+            im = Image.open(path)
+            b.add(path, im)
+    b.build()
 
 if __name__ == '__main__':
-	bundle("/Applications/TextMate.app/Contents/Resources")
+    bundle("/Applications/TextMate.app/Contents/Resources")
--- src/bundler/test.py
+++ src/bundler/test.py
@@ -1,44 +1,48 @@
 from __init__ import Bundle
+import Image
+import ImageDraw
 
-"""
-[TODO] less ridiculous test
-"""
+
 def testCroix():
-	cpt = 0
-	def imageFactory(size):
-		global cpt
-		cpt += 1
-		im = Image.new("RGB", size)
-		w,h = size
-		w -= 1
-		h -=1
-		draw = ImageDraw.Draw(im)
-		#rectangle
-		draw.line((0,0, 0,h), fill=128)
-		draw.line((0,h, w,h), fill=128)
-		draw.line((w,h, w,0), fill=128)
-		draw.line((w,0, 0,0), fill=128)
-		#cross
-		draw.line((0,0, w,h), fill=128)
-		draw.line((0,h, w,0), fill=128)
-		del draw
-		im.save("%i.png" % cpt)
-		return im
+    """
+    [TODO] less ridiculous test
+    """
+    cpt = 0
 
-	b = Bundle()
-	b.add("bandeau", imageFactory((320, 24)))
-	b.add("icone1" ,imageFactory((24, 24)))
-	b.add("icone2" ,imageFactory((24, 24)))
-	b.add("icone3" ,imageFactory((24, 24)))
-	b.add("icone4" ,imageFactory((24, 24)))
-	b.add("icone5" ,imageFactory((24, 24)))
-	b.add("GrosIcone" ,imageFactory((32, 32)))
-	b.add("pub" ,imageFactory((128, 64)))
-	b.add("pub" ,imageFactory((150, 60)))
+    def imageFactory(size):
+        global cpt
+        cpt += 1
+        im = Image.new("RGB", size)
+        w, h = size
+        w -= 1
+        h -= 1
+        draw = ImageDraw.Draw(im)
+        #rectangle
+        draw.line((0, 0, 0, h), fill=128)
+        draw.line((0, h, w, h), fill=128)
+        draw.line((w, h, w, 0), fill=128)
+        draw.line((w, 0, 0, 0), fill=128)
+        #cross
+        draw.line((0, 0, w, h), fill=128)
+        draw.line((0, h, w, 0), fill=128)
+        del draw
+        im.save("%i.png" % cpt)
+        return im
 
-	print b
-	print b.larger()
-	b.build()
+    b = Bundle()
+    b.add("bandeau", imageFactory((320, 24)))
+    b.add("icone1", imageFactory((24, 24)))
+    b.add("icone2", imageFactory((24, 24)))
+    b.add("icone3", imageFactory((24, 24)))
+    b.add("icone4", imageFactory((24, 24)))
+    b.add("icone5", imageFactory((24, 24)))
+    b.add("GrosIcone", imageFactory((32, 32)))
+    b.add("pub", imageFactory((128, 64)))
+    b.add("pub", imageFactory((150, 60)))
+
+    print b
+    print b.larger()
+    b.build()
 
 if __name__ == '__main__':
-	testCroix()
+    testCroix()
